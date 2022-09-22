45d665e7b606834628ff5f87bc78089f4ff3b820
Sooraj Philip <soorajphilip97@gmail.com>
2022-09-22 12:46:33 +0530

    Add bootstrap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,3 +6,23 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("@popperjs/core")
+
+import "bootstrap"
+
+import { Tooltip, Popover } from "bootstrap"
+
+require("../stylesheets/application.scss")
+
+document.addEventListener("turbolinks:load", () => {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
