9f934160700572aef8cbf7a7b65adacdbd62dd91
Sooraj Philip <soorajphilip97@gmail.com>
2022-09-24 10:23:27 +0530

    Code cleanup

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -5,6 +5,6 @@ class HistoryController < ApplicationController
   def search_query
     @start_date = params[:range].split(' - ').first
     @end_date = params[:range].split(' - ').last
-    @parking_history = ParkingHistory.where(:date => @start_date..@end_date)
+    @parking_history = ParkingHistory.where(date: @start_date..@end_date)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,5 @@
 module ApplicationHelper
 
-  def toastr_flash
-    flash.each_with_object([]) do |(type, message), flash_messages|
-      type = 'success' if type == 'notice'
-      type = 'error' if type == 'alert'
-      text = "<script>toastr.#{type}('#{message}', '', { closeButton: true, progressBar: true, timeOut: 2500 })</script>"
-      flash_messages << text.html_safe if message
-    end.join("\n").html_safe
-  end
-
   def active_link url
     'active' if current_page?(url)
   end
